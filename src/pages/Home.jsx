0b7eae618e57bd6d665661627367fcc1036a8dc5
M. Naderi <PrettySimpleDeveloper@gmail.com>
2023-11-04 21:00:06 +0330

    hero banner added to Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,14 @@
+import "./Home.scss";
+
 function Home() {
   return (
-    <div>
-      <section>d </section>
-    </div>
+    <section className="banner__bg">
+      <div className="container">
+        <h1 className="banner__text">
+          The habit tracker you get into the habit of using
+        </h1>
+      </div>
+    </section>
   );
 }
 
