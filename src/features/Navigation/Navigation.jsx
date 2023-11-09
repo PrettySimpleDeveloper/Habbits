daf1b55102e7df840e6c38e0d461f773477d25a6
M. Naderi <PrettySimpleDeveloper@gmail.com>
2023-11-09 19:35:25 +0330

    Navigation component converted to styled component

--- src/features/Navigation/Navigation.jsx
+++ src/features/Navigation/Navigation.jsx
@@ -1,32 +1,124 @@
+import styled from "styled-components";
 import { Link, Outlet } from "react-router-dom";
 import "./Navigation.scss";
+
+const Header = styled.header`
+  height: 3.4rem;
+  display: block;
+  position: sticky;
+  margin: 1rem auto !important;
+  background-color: var(--color-grey-50);
+  box-shadow: var(--shadow-blue-lg);
+  border-radius: var(--border-radius-md);
+  width: 100%;
+  border: 1px solid var(--color-brand-100);
+  z-index: 7777;
+`;
+
+const BrandContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const BrandImage = styled.img`
+  width: 2.6rem;
+`;
+
+const BrandName = styled.h1`
+  font-size: 1.7rem;
+  font-weight: 800;
+  margin: 0 0 0 1rem;
+  color: #545454;
+`;
+
+const Nav = styled.nav`
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 0.7rem;
+`;
+
+const NavList = styled.ul`
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  justify-content: space-around;
+`;
+
+const StyledNavLink = styled(Link)`
+  &:link,
+  &:visited {
+    display: flex;
+    align-items: center;
+    gap: 1.2rem;
+
+    color: var(--color-grey-600);
+
+    padding: 0.8em;
+    border-bottom: 3px solid transparent;
+    transition: all 0.3s;
+  }
+
+  &:hover {
+    color: var(--color-brand-500);
+    border-bottom: 3px solid var(--color-brand-500);
+  }
+
+  /* This works because react-router places the active class on the active NavLink */
+  /*
+  &:hover,
+  &:active,
+  &.active:link,
+  &.active:visited {
+    color: var(--color-grey-800);
+    background-color: var(--color-grey-50);
+    border-radius: var(--border-radius-sm);
+  }
+
+   & svg {
+    width: 2.4rem;
+    height: 2.4rem;
+    color: var(--color-grey-400);
+    transition: all 0.3s;
+  }
+
+  &:hover svg,
+  &:active svg,
+  &.active:link svg,
+  &.active:visited svg {
+    color: var(--color-brand-600);
+  } */
+`;
 
 function Navigation() {
   return (
     <>
-      <header className="header container">
-        <nav className="header__nav">
-          <div className="logo__container">
+      <Header className="container">
+        <Nav>
+          <BrandContainer>
             <Link to="/">
-              <img src="./logo.svg" alt="logo" className="logo__image" />
+              <BrandImage src="./logo.svg" alt="logo" />
             </Link>
             <Link to="/">
-              <h1 className="logo__text">Habbits</h1>
+              <BrandName>Habbits</BrandName>
             </Link>
-          </div>
-          <ul className="header__list">
-            <Link className="header__link" to="/">
-              <li className="header__listItem">Home</li>
-            </Link>
-            <Link className="header__link" to="/app">
-              <li className="header__listItem">App</li>
-            </Link>
-            <Link className="header__link" to="/about">
-              <li className="header__listItem">About Us</li>
-            </Link>
-          </ul>
-        </nav>
-      </header>
+          </BrandContainer>
+          <NavList className="header__list">
+            <li>
+              <StyledNavLink to="/">Home</StyledNavLink>
+            </li>
+            <li>
+              <StyledNavLink to="/app">App</StyledNavLink>
+            </li>
+            <li>
+              <StyledNavLink to="/about">About Us</StyledNavLink>
+            </li>
+          </NavList>
+        </Nav>
+      </Header>
 
       <Outlet />
     </>
